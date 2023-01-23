7778d1c212f54bb017fb77608f25c349efed9d1b
etch1000 <hmoroliya@ariqt.com>
2023-01-23 19:27:09 +0530

    Tenth problem solved from Rust Code Challenges by Tim McNamara

---
+++ calculate_the_number_of_weeks_between_two_dates/src/main.rs
@@ -0,0 +1,36 @@
+use chrono::NaiveDate;
+
+fn weeks_between(a: &str, b: &str) -> i32 {
+    // b is before a then negative number
+    let t1 = NaiveDate::parse_from_str(a, "%Y-%m-%d").unwrap();
+    let t2 = NaiveDate::parse_from_str(b, "%Y-%m-%d").unwrap();
+
+    let diff = t2 - t1;
+
+    let num_weeks = diff.num_weeks();
+
+    num_weeks as i32
+}
+
+fn main() {
+    let n_weeks = weeks_between("2010-01-21", "2010-10-21");
+    println!("hello: {}", n_weeks);
+}
+
+#[test]
+fn same_day() {
+    let n_weeks = weeks_between("1010-10-10", "1010-10-10");
+    assert_eq!(n_weeks, 0);
+}
+
+#[test]
+fn one_week() {
+    let n_weeks = weeks_between("1010-10-10", "1010-10-18");
+    assert_eq!(n_weeks, 1);
+}
+
+#[test]
+fn past() {
+    let n_weeks = weeks_between("1010-10-18", "1010-10-10");
+    assert_eq!(n_weeks, -1);
+}
