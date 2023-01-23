056eefed953888c9405957fc59709915184cc387
etch1000 <hmoroliya@ariqt.com>
2023-01-23 17:45:10 +0530

    Ninth problem solved from Rust Code Challenges by Tim McNamara

--- sum_a_list_of_numbers_with_missing_values/target/debug/deps/sum_a_list_of_numbers_with_missing_values-534735a08341cf38.d
+++
@@ -1,5 +0,0 @@
-/home/rootkill/rust_all/rust_code_challenges/sum_a_list_of_numbers_with_missing_values/target/debug/deps/sum_a_list_of_numbers_with_missing_values-534735a08341cf38: src/main.rs
-
-/home/rootkill/rust_all/rust_code_challenges/sum_a_list_of_numbers_with_missing_values/target/debug/deps/sum_a_list_of_numbers_with_missing_values-534735a08341cf38.d: src/main.rs
-
-src/main.rs:
--- sum_a_list_of_numbers_with_missing_values/target/debug/deps/sum_a_list_of_numbers_with_missing_values-9d0dd48e9121b31f.d
+++
@@ -1,5 +0,0 @@
-/home/rootkill/rust_all/rust_code_challenges/sum_a_list_of_numbers_with_missing_values/target/debug/deps/sum_a_list_of_numbers_with_missing_values-9d0dd48e9121b31f.rmeta: src/main.rs
-
-/home/rootkill/rust_all/rust_code_challenges/sum_a_list_of_numbers_with_missing_values/target/debug/deps/sum_a_list_of_numbers_with_missing_values-9d0dd48e9121b31f.d: src/main.rs
-
-src/main.rs:
